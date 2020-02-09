3bc56cc88798148509e72eb9c6145cee17739fbc
yaphyao <0605KLI>
2020-02-09 13:13:14 +0800

    股票下降 邮件通知

--- src/main/java/com/jxnu/finance/crawler/component/StockNetWorthService.java
+++ src/main/java/com/jxnu/finance/crawler/component/StockNetWorthService.java
@@ -2,8 +2,11 @@ package com.jxnu.finance.crawler.component;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
 import com.jxnu.finance.store.entity.stock.StockExtra;
 import com.jxnu.finance.store.entity.stock.StockNetWorth;
+import com.jxnu.finance.store.entity.strategy.Mail;
+import com.jxnu.finance.store.mapper.MailStore;
 import com.jxnu.finance.store.mapper.StockExtraStore;
 import com.jxnu.finance.store.mapper.StockNetWorthStore;
 import com.jxnu.finance.utils.*;
@@ -20,6 +23,8 @@ public class StockNetWorthService {
     private StockNetWorthStore stockNetWorthStore;
     @Autowired
     private StockExtraStore stockExtraStore;
+    @Autowired
+    private MailStore mailStore;
 
     public void netWorth(String stockCode) {
         if (StringUtil.isBank(stockCode)) {
@@ -40,21 +45,10 @@ public class StockNetWorthService {
         if (jsonArray == null || jsonArray.isEmpty()) {
             return;
         }
-        Double maxNetWorth = maxNetWorth(stockCode);
+
         List<StockNetWorth> stockNetWorths = new ArrayList<StockNetWorth>();
         JSONObject lastNetWorthJson = jsonArray.getJSONObject(0);
-        if (maxNetWorth != null && lastNetWorthJson != null) {
-            Double lastNetWorth = lastNetWorthJson.getDoubleValue("F002N");
-            Double divide = CalculateUtil.divide(maxNetWorth - lastNetWorth, maxNetWorth, 2);
-            if (divide > 0.3) {
-                StringBuffer stringBuffer = new StringBuffer();
-                stringBuffer.append("<html><head></head><body>");
-                stringBuffer.append(stockExtra.getStockName() + ": <a href=\"" + stockExtra.getStockUrl() + "\">" + "下降比例:" + divide);
-                stringBuffer.append("</body></html>");
-                MailUtil.sendmail(stockExtra.getStockName() + "净值下降通知", stringBuffer.toString());
-            }
-        }
-
+        mail(stockExtra, lastNetWorthJson);
         for (Object object : jsonArray) {
             StockNetWorth stockNetWorth = new StockNetWorth();
             JSONObject jsonObject = (JSONObject) object;
@@ -67,14 +61,21 @@ public class StockNetWorthService {
         stockNetWorthStore.insert(stockNetWorths);
     }
 
-
     /**
-     * 计算3个月内最大净值
+     * 计算3个月内净值下降30%的股票
      *
-     * @param stockCode
-     * @return
+     * @param stockExtra
+     * @param lastNetWorthJson
      */
-    public Double maxNetWorth(String stockCode) {
+    public void mail(StockExtra stockExtra, JSONObject lastNetWorthJson) {
+        String stockCode = stockExtra.getStockCode();
+        /**
+         * 邮件次数控制
+         */
+        Integer count = mailStore.queryMail(stockCode, "1");
+        if (count > 0) {
+            return;
+        }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String endTime = TimeUtil.current(simpleDateFormat);
         String startTime = TimeUtil.current(simpleDateFormat, Calendar.DAY_OF_YEAR, -90);
@@ -82,6 +83,25 @@ public class StockNetWorthService {
         params.put("startTime", startTime);
         params.put("endTime", endTime);
         params.put("stockCode", stockCode);
-        return stockNetWorthStore.selectMaxNetWorth(params);
+        Double maxNetWorth = stockNetWorthStore.selectMaxNetWorth(params);
+        if (maxNetWorth != null && lastNetWorthJson != null) {
+            Double lastNetWorth = lastNetWorthJson.getDoubleValue("F002N");
+            Double divide = CalculateUtil.divide(maxNetWorth - lastNetWorth, maxNetWorth, 2);
+            if (divide > 0.3) {
+                StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append("<html><head></head><body>");
+                stringBuffer.append(stockExtra.getStockName() + ": <a href=\"" + stockExtra.getStockUrl() + "\">" + "下降比例:" + divide);
+                stringBuffer.append("</body></html>");
+                MailUtil.sendmail(stockExtra.getStockName() + "净值下降通知", stringBuffer.toString());
+                /**
+                 * 记录发送的邮件
+                 */
+                Mail mail = new Mail();
+                mail.setCode(stockCode);
+                mail.setTime(endTime);
+                mail.setType("1");
+                mailStore.insert(Lists.newArrayList(mail));
+            }
+        }
     }
 }
